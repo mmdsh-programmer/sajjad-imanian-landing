553e64ec5cf3d67a76a6bc22a4a9e383b384b224
Mohammad Shakeri <mohammadshakeri@Mohammads-MacBook-Air.local>
2025-05-18 01:13:03 +0330

    feat: add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
+import About from "@/components/about";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <About />
     </main>
   );
 }
---
+++ components/about.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import { UnderlineIcon } from "@/assets/svg";
+
+const About = () => (
+  <section className="pt-[104px] pb-7">
+    <div className="container">
+      <article>
+        <figure className="w-[328px] aspect-square relative float-left mr-4">
+          <Image
+            src="/about.jpg"
+            alt="About Sajjad Imanian"
+            fill
+            className="object-cover"
+            priority
+          />
+        </figure>
+        <header>
+          <h2 className="font-bold text-[#272727] text-[42px] leading-[44px] clear-none pt-[246px]">
+            My journey in the world of social media and content creation began
+            at the age of 16 with one of Iran&apos;s prominent newspapers, and I
+            have continued to evolve and thrive in this field over the past
+            eight years.
+          </h2>
+        </header>
+        <div className="clear-both" />
+      </article>
+      <aside className="w-full flex justify-end mt-20">
+        <div className="max-w-[calc(390px+114px+114px)] py-[27px] px-[114px] text-center">
+          <p className="font-instrument-serif text-[40px] leading-[42px] text-center text-dark-yellow">
+            I have successfully grown the AFC Farsi Instagram account to an
+            impressive total of
+          </p>
+          <mark className="font-instrument-serif text-[40px] leading-[42px] text-center text-dark-yellow italic bg-transparent mt-2 block">
+            3.5 million followers
+          </mark>
+          <UnderlineIcon className="mx-auto" />
+        </div>
+      </aside>
+    </div>
+  </section>
+);
+
+export default About;
